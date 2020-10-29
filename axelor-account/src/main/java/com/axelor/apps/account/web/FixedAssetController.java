b3a06ff77f7a7918432276439609e1dabcb6db46
Jean-Arthur <j.boulet@axelor.com>
2020-10-29 14:14:41 +0100

    Fixed issue when validating a Fixed Asset

--- axelor-account/src/main/java/com/axelor/apps/account/web/FixedAssetController.java
+++ axelor-account/src/main/java/com/axelor/apps/account/web/FixedAssetController.java
@@ -3,17 +3,15 @@
  *
  * Copyright (C) 2020 Axelor (<http://axelor.com>).
  *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License, version 3, as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 package com.axelor.apps.account.web;
 
@@ -74,7 +72,9 @@ public class FixedAssetController {
   }
 
   public void validate(ActionRequest request, ActionResponse response) {
-    FixedAsset fixedAsset = request.getContext().asType(FixedAsset.class);
+    FixedAsset fixedAsset =
+        Beans.get(FixedAssetRepository.class)
+            .find(request.getContext().asType(FixedAsset.class).getId());
     if (fixedAsset.getStatusSelect() == FixedAssetRepository.STATUS_DRAFT) {
       try {
         Beans.get(FixedAssetService.class).validate(fixedAsset);
